6b225a248c46f2d01358f3bdc05997cc25771005
aymericGodivier <44438326+aymericGodivier@users.noreply.github.com>
2024-03-27 17:13:08 +0100

    header and footer

---
+++ components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer>
+            <img src="../../src/assets/LOGO_KASA_WHITE.svg" alt="Logo white"></img>
+            <p>©2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ components/header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <img src="../../src/assets/LOGO_KASA.svg" alt="Logo"></img>
+            <nav>
+                <li>Accueil</li>
+                <li>A propos</li>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
