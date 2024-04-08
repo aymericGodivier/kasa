9dce9539411c8d45c0a574fafd0f491fa1ec28de
aymericGodivier <44438326+aymericGodivier@users.noreply.github.com>
2024-04-08 15:38:13 +0200

    location LocationDetailsContext

--- contexts/LocationDetailsContext.jsx
+++ contexts/LocationDetailsContext.jsx
@@ -4,28 +4,35 @@ import { useParams } from 'react-router-dom';
 const LocationContext = createContext();
 
 export const LocationProvider = ({ children }) => {
-  const { id } = useParams();
+  
+
   const [locationDetails, setLocationDetails] = useState(null);
 
-  useEffect(() => {
-    const fetchDetails = async (url) => {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setLocationDetails(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const fetchDetails = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/properties/${id}`);
+      const data = await response.json();
+      console.log(data)
+      return data;
 
-    fetchDetails(`http://localhost:8080/api/properties/${id}`);
-  }, [id]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+useEffect(() => {
+    
+
+});
 
   return (
-    <LocationContext.Provider value={{ locationDetails, setLocationDetails }}>
+    <LocationContext.Provider value={{fetchDetails,locationDetails}}>
       {children}
     </LocationContext.Provider>
   );
 };
 
-export const useLocation = () => useContext(LocationContext);
+export const useLocation = () => {
+  const context = useContext(LocationContext)
+  return context
+};
