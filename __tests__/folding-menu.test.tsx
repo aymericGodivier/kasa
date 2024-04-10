516e288f876f1c9391f9aabb4c0fa61feceb3ffb
aymericGodivier <44438326+aymericGodivier@users.noreply.github.com>
2024-04-10 12:42:45 +0200

    test setup fonctionnel

---
+++ __tests__/folding-menu.test.tsx
@@ -0,0 +1,11 @@
+import  FoldingMenu  from '../components/folding-menu'
+import { describe, it, expect } from 'vitest'
+
+describe('creating the folding menu ', () => {
+    it('sould work'), ()=> {
+        const result = 4
+        const test = 4
+        expect(test).toEqual(result)
+    }
+
+})
